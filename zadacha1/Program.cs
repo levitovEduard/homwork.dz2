f776c10cd43a9ba012b3a5f7d26602f822e63782
Eduard <levitov.eduard@gmail.com>
2022-12-08 18:46:41 +0200

    zadacha 1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+{
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        num= num /= 10;
+        num %= 10;
+        Console.WriteLine(num);
+    }
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+ }
