f9e14ebf25be185046dea09fb4e8000466a1c64e
Eduard <levitov.eduard@gmail.com>
2022-12-08 19:25:40 +0200

    zadacha 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+{
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            if (num < 6)
+            {
+                Console.WriteLine("нет");
+            }
+            else
+            {
+                Console.WriteLine("да");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
